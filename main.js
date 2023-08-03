6f43e1e11beaa04dbd9729aa252a1056f3ba6748
Nikhitha R Karanth <nikhitharkaranth@gmail.com>
2023-08-04 01:25:02 +0530

    section 12(after styling)

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let getProjects = () => {
 
 
 let buildProjects = (projects) => {
-    let projectsWrapper = document.getElementById('projects-wrapper')
+    let projectsWrapper = document.getElementById('projects--wrapper')
     
     for (let i=0; projects.length>i; i++){
         let project = projects[i]
@@ -21,8 +21,23 @@ let buildProjects = (projects) => {
         // console.log(project)
 
         let projectCard=`
-            <div>
-                <p>${project.title}</p>
+            <div class="project--card">
+                <img src="http://127.0.0.1:8000${project.featured_image}"/>
+
+                <div>
+                    <div class="card--header">
+
+                        <h3>${project.title}</h3>
+                        <strong class="vote--option">&#43;</strong>
+                        <strong class="vote--option">&#8722;</strong>
+                    
+                    </div>
+
+                    <i>${project.vote_ratio}% Positive feedback</i>
+                    <p>${project.description.substring(0,150)}</p>
+
+                </div>
+
             </div>
         `
 
